28257304c414bd723a670f78803cbcde5084aa8b
b65t <iislamm6s7s5@tutamail.com>
2024-12-03 11:30:14 +0100

    Add files via upload

---
+++ .config/sway/swww.sh
@@ -0,0 +1,14 @@
+positions=("top-left" "top-right")
+position=$(shuf -e ${positions[@]} -n1)
+
+if [[ ! -z $1 ]]; then
+  swww img "$1" 
+    --transition-type grow 
+    --transition-pos "$position" 
+    --transition-duration 2 
+    --transition-fps 60
+    --format xrgb
+else
+  echo "no wallpaper specified"
+fi
+
